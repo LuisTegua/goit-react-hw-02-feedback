55a1f7bb1b7f52907917a274394ccbb343111f3e
LuisETeguaS <tegualuis2@gmail.com>
2023-07-28 04:54:28 -0500

    Feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
+import React from "react";
+import css from './app.module.css';
+
+class Counter extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  incg = () => {
+    this.setState({ good: this.state.good + 1 });
+  };
+  incn = () => {
+    this.setState({ neutral: this.state.neutral + 1 });
+  };
+  incb = () => {
+    this.setState({ bad: this.state.bad + 1 });
+  };
+  total = () => {
+    return Object.values(this.state).reduce((a, b) => a + b, 0)
+  }
+  porcentaje = () => {
+    return Math.trunc(this.total() > 0 ?(this.state.good / this.total()) * 100 :0);
+  }
+  render() {
+    const { good, neutral, bad } = this.state;
+    const a = this.total;
+    return (
+      <div className={css.container}>
+        <h2 className={css.title}>Please leave feedback</h2>
+        <button type="button" className={css.btn} onClick={this.incg}>
+          👍
+        </button>
+        <button type="button" className={css.btn} onClick={this.incn}>
+          🤷
+        </button>
+        <button type="button" className={css.btn} onClick={this.incb}>
+          👎
+        </button>
+        {this.total() > 0
+          ?<>
+            <h3>Statistics</h3>
+            <p>Good:{good}</p>
+            <p>Neutral: {neutral} </p>
+            <p>Bad: {bad}</p>
+            <p>Total: {this.total()}</p>
+            <p>Porcentaje: {this.porcentaje()}%</p>
+          </>
+          :<p>There is no feedback</p>
+        }
+      </div>
+    );
+  }
+}
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Counter/>
+    </>
   );
 };
